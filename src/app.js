e436936b8ac08c28a7b29afcc578cf1ed13f3322
Gustavo Gomes <123559372+Gusdev06@users.noreply.github.com>
2023-08-25 19:58:56 -0300

    fetch cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import  express  from 'express'; // importanto o express 
 const app = express() // instaciando o app 
 
+import fs from 'fs';
+import https from 'https';
+import cors from 'cors';
+
 import routes from './routes.js'
 
 app.use(express.json());
+app.use(cors());
 
 app.use(routes)
 
 
 app.listen(3000, () =>console.log('Api initialized')) // definindo a porta 
+
+https.createServer({
+    cert: fs.readFileSync('src/SSL/code.crt'),
+    key: fs.readFileSync('src/SSL/code.key')
+}, app).listen(3002, () =>console.log('Api rodando'));
--- src/configDB.js
+++ src/configDB.js
@@ -1,9 +1,12 @@
-import sqlite3 from 'sqlite3' //  Importa o módulo sqlite3, que é um driver para interagir com bancos de dados
-import { open } from 'sqlite' //Importa a função open do módulo sqlite. A função open é usada para abrir conexões com bancos de dados SQLite
+import sqlite3 from 'sqlite3' 
+import { open } from 'sqlite' 
 
-export async function openDb () { //Define uma função assíncrona chamada openDb que será exportada para ser usada em outros arquivos. Essa função é responsável por abrir uma conexão com o banco de dados SQLite.
-  return open({ // Dentro da função openDb, está sendo chamada a função open que foi importada anteriormente. Esta função é usada para criar uma conexão com o banco de dados SQLite.
-    filename: './database.db', // Aqui, você está definindo o nome e o local do arquivo de banco de dados SQLite. Neste caso, o banco de dados será criado no diretório temporário /tmp com o nome database.db.
-    driver: sqlite3.Database //Aqui, você está especificando o driver a ser usado para interagir com o banco de dados. O sqlite3.Database é o driver que está sendo usado para trabalhar com o SQLite.
+export async function openDb () { 
+  return open({ 
+    filename: './database.db', 
+    driver: sqlite3.Database 
   })
 }
+
+
+
