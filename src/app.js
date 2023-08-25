b41a7115e810d42dd8fc2b3b4742c7e4b033017c
Gustavo Gomes <123559372+Gusdev06@users.noreply.github.com>
2023-08-25 20:43:58 -0300

    corrigino origin cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,6 @@ import routes from './routes.js'
 app.use(express.json());
 app.use(cors({
     origin: '*',
-
 }));
 
 app.use(routes)
@@ -21,4 +20,4 @@ app.listen(3000, () =>console.log('Api initialized')) // definindo a porta
 https.createServer({
     cert: fs.readFileSync('src/SSL/code.crt'),
     key: fs.readFileSync('src/SSL/code.key')
-}, app).listen(3002, () =>console.log('Api rodando'));
+}, app).listen(3002, () =>console.log('Api rodando'))
