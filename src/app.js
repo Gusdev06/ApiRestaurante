e48ea8b7ff1dc8426650194215e7111716098be9
Gustavo Gomes <123559372+Gusdev06@users.noreply.github.com>
2023-08-25 20:41:27 -0300

    origin cors

--- src/app.js
+++ src/app.js
@@ -9,9 +9,8 @@ import routes from './routes.js'
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://127.0.0.1:5500',
-    methods: ['GET', 'POST'],
-    allowedHeaders: 'Content-Type'
+    origin: '*',
+
 }));
 
 app.use(routes)
@@ -19,3 +18,7 @@ app.use(routes)
 
 app.listen(3000, () =>console.log('Api initialized')) // definindo a porta 
 
+https.createServer({
+    cert: fs.readFileSync('src/SSL/code.crt'),
+    key: fs.readFileSync('src/SSL/code.key')
+}, app).listen(3002, () =>console.log('Api rodando'));
